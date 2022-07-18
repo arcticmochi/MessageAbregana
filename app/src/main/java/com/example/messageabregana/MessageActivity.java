4061f32952d594b3e91824d4849678046e166871
Laurence_A <abre.svnoj@gmail.com>
2022-07-18 19:14:43 +0800

    Milestone 1: Use intent

--- app/src/main/java/com/example/messageabregana/MessageActivity.java
+++ app/src/main/java/com/example/messageabregana/MessageActivity.java
@@ -1,31 +1,24 @@
 package com.example.messageabregana;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MessageActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.messageabregana.MESSAGE";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        btnSendListenerMethod();
-    }
-
-    private void btnSendListenerMethod() {
-        Button btnSend = findViewById(R.id.btnSend);
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText etMessage = findViewById(R.id.etMessage);
-                String message = etMessage.getText().toString();
-            }
-        });
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView tvMessage = findViewById(R.id.tvMessage);
+        tvMessage.setText(message);
     }
 }
