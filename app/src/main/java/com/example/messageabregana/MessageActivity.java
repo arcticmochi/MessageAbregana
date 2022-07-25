1ddd945943b2281b9574e8d63ad4b874d2aac4e9
Laurence_A <abre.svnoj@gmail.com>
2022-07-25 16:56:21 +0800

    Milestone 5: Use preferences

--- app/src/main/java/com/example/messageabregana/MessageActivity.java
+++ app/src/main/java/com/example/messageabregana/MessageActivity.java
@@ -1,6 +1,11 @@
 package com.example.messageabregana;
 
+import static com.example.messageabregana.MainActivity.PREF_NIGHT;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,16 +14,22 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MessageActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.messageabregana.MESSAGE";
     public static final String EXTRA_RESULT = "com.example.messageabregana.RESULT";
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        SharedPreferences preferences = getSharedPreferences("Abregana", Activity.MODE_PRIVATE);
+        boolean night = preferences.getBoolean(PREF_NIGHT, false);
+        initializeNightMode(night);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(EXTRA_MESSAGE);
@@ -26,6 +37,24 @@ public class MessageActivity extends AppCompatActivity {
         tvMessage.setText(message);
 
         btnSaveListenerMethod();
+    }
+
+    private void initializeNightMode(boolean night) {
+        ConstraintLayout clMessage = findViewById(R.id.clMessage);
+        int textviews[] = {R.id.tvLabel, R.id.tvMessage, R.id.rbGood, R.id.rbBad};
+        if (night) {
+            clMessage.setBackgroundColor(Color.BLACK);
+            for (int tv : textviews) {
+                TextView v = findViewById(tv);
+                v.setTextColor(Color.WHITE);
+            }
+        } else {
+            clMessage.setBackgroundColor(Color.WHITE);
+            for (int tv : textviews) {
+                TextView v = findViewById(tv);
+                v.setTextColor(Color.BLACK);
+            }
+        }
     }
 
     private void btnSaveListenerMethod() {
